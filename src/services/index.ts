78af2c404379c05faae3d4c987384a49ebba76bb
Your Name <you@example.com>
2020-05-04 17:22:16 +0530

    implemantation of stock items

--- src/services/index.ts
+++ src/services/index.ts
@@ -8,6 +8,10 @@ export * from './project.api';
 export * from './project.service';
 export * from './task.api';
 export * from './task.service';
+export * from './stock-items.api';
+export * from './stock-items-mock.service';
+
+
 
 Container.bind(TaskApi).to(TaskService).scope(Scope.Singleton);
 Container.bind(ProjectApi).to(ProjectService).scope(Scope.Singleton);
